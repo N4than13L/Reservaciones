285401fe0102732421186e14624ed8f4ba2dbdb9
Nathaniel <natharevolution@gmail.com>
2023-05-08 21:56:26 -0400

    cambiando frontend de reactjs a angular

--- reservaciones-hoteleras/src/Helpers/Global.jsx
+++
@@ -1,3 +0,0 @@
-export const Global = {
-  url: "http://localhost/reservaciones/api-rest-reservas/public/api",
-};
--- reservaciones-hoteleras/src/components/Booking/Bookings.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Bookings = () => {
-  return (
-    <>
-      <section className="container">Bookings</section>
-    </>
-  );
-};
